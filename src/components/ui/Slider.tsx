8a2e0089ed2ee68a4a82e11b2351c865b0172667
_nderscore <_@nderscore.com>
2022-09-05 16:55:38 -0400

    feat: slider percentage variant

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,20 +1,34 @@
 import classNames from 'classnames';
-import type { JSX, VoidComponent } from 'solid-js';
+import { createMemo, JSX, VoidComponent } from 'solid-js';
 
 export type SliderProps = {
   class?: string;
   label: string;
   max: number;
   min: number;
-  step?: number;
   name: string;
+  percentage?: boolean;
+  step?: number;
   value: number;
   set: (next: number) => void;
 };
 
 export const Slider: VoidComponent<SliderProps> = (props) => {
-  const onInput: JSX.EventHandler<HTMLInputElement, Event> = (e) =>
-    props.set((props.step ?? 1 < 1 ? parseFloat : parseInt)(e.currentTarget.value));
+  const onInput: JSX.EventHandler<HTMLInputElement, Event> = (e) => {
+    const decimalStep = props.step ?? 1 < 1;
+    const inputValue = e.currentTarget.value;
+    if (props.percentage) {
+      props.set(parseInt(inputValue) / 100);
+    } else {
+      props.set((decimalStep ? parseFloat : parseInt)(inputValue));
+    }
+  };
+  const value = createMemo(() => {
+    if (props.percentage) {
+      return Math.round(props.value * 100);
+    }
+    return props.value;
+  });
 
   return (
     <div class="form-control">
@@ -29,10 +43,13 @@ export const Slider: VoidComponent<SliderProps> = (props) => {
           min={props.min}
           name={props.name}
           step={props.step}
-          value={props.value}
+          value={value()}
           onInput={onInput}
         />
-        <div class="w-16 text-center">{props.value}</div>
+        <div class="w-16 text-center">
+          {value()}
+          {props.percentage && '%'}
+        </div>
       </div>
     </div>
   );
