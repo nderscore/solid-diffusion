02d67204742542d4dbb702fcf25be8619967e548
_nderscore <_@nderscore.com>
2022-09-06 20:53:13 -0400

    feat: open images in a full screen modal on click

--- src/components/Gallery/DreamCard.tsx
+++ src/components/Gallery/DreamCard.tsx
@@ -2,7 +2,7 @@ import { useI18n } from '@solid-primitives/i18n';
 import classNames from 'classnames';
 import { Icon } from 'solid-heroicons';
 import { ellipsisHorizontal, informationCircle } from 'solid-heroicons/solid-mini';
-import { createSignal, Match, Switch, VoidComponent } from 'solid-js';
+import { createSignal, Match, Show, Switch, VoidComponent } from 'solid-js';
 
 import { Dream } from '~/types';
 
@@ -14,6 +14,7 @@ export type DreamCardProps = {
 };
 
 export const DreamCard: VoidComponent<DreamCardProps> = (props) => {
+  const [showModalImage, setShowModalImage] = createSignal(false);
   const [showInfo, setShowInfo] = createSignal(false);
   const [showActions, setShowActions] = createSignal(false);
   const [t] = useI18n();
@@ -22,7 +23,7 @@ export const DreamCard: VoidComponent<DreamCardProps> = (props) => {
 
   return (
     <div role="group" class="card card-compact card-dreamlog bg-base-200 shadow-xl">
-      <button class="btn-glass">
+      <button class="btn-glass" onClick={() => setShowModalImage(true)}>
         <figure class="figure-dreamlog bg-neutral checkerboard-neutral w-dreamlog-grid-width h-dreamlog-grid-width">
           <img
             src={props.dream.result}
@@ -65,6 +66,19 @@ export const DreamCard: VoidComponent<DreamCardProps> = (props) => {
           <DreamInfo dream={props.dream} />
         </Match>
       </Switch>
+      <Show when={showModalImage()}>
+        <div
+          class="bg-neutral checkerboard-neutral fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50"
+          onClick={() => setShowModalImage(false)}
+        >
+          <img
+            src={props.dream.result}
+            alt={props.dream.settings.prompt}
+            ref={image}
+            class="shadow-lg max-h-full max-w-full object-contain"
+          />
+        </div>
+      </Show>
     </div>
   );
 };
